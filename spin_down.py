2dc731ae2e761d9f2ae10c3a6c00521e646bb178
Lun Song <hustsonglun@gmail.com>
2022-05-27 07:14:12 +0800

    foo

--- spin_down.py
+++ spin_down.py
@@ -73,37 +73,37 @@ get_m2 = lambda B=1e11, R=1e4:\
         (2*np.pi*R**3*B)**2 / (4*np.pi*1e-7) * 1e13 * g*cm**5*s**-2
 
 
-Omega_e_at_M0_E_0 = interpolate((M0,E), (Omega,e), e<=e_0)
-Omega_e_at_M0_E_1 = interpolate((M0,E), (Omega,e), e>=e_0)
-J_V_at_M0_E_0 = interpolate((M0,E), (J,V), e<=e_0)
-E_V_at_M0_J_1 = interpolate((M0,J), (E,V), e>=e_1)
+Omega_e_at_M0_T_0 = interpolate((M0,T), (Omega,e), e<=e_0)
+Omega_e_at_M0_T_1 = interpolate((M0,T), (Omega,e), e>=e_0)
+J_V_at_M0_T_0 = interpolate((M0,T), (J,V), e<=e_0)
+T_V_at_M0_J_1 = interpolate((M0,J), (T,V), e>=e_1)
 
-def evolve(M0,E,dt=1e6,m2=get_m2(),N=10000):
+def evolve(M0,T,dt=1e6,m2=get_m2(),N=10000):
     """params are in geometric units"""
     t = dt
     quark = False
     try:
-        Omega, e = Omega_e_at_M0_E_0(M0,E)
+        Omega, e = Omega_e_at_M0_T_0(M0,T)
     except InterpError:
         quark = True
-        Omega, e = Omega_e_at_M0_E_1(M0,E)
+        Omega, e = Omega_e_at_M0_T_1(M0,T)
     try:
         for step in range(N):
-            if step%100==0:print(f"{Omega}{' '*10}{T}{' '*10}", end="\n")
+            if step%1==0:print(f"{Omega:e} {e:e}", end="\n")
             if quark:
-                Omega,e = Omega_e_at_M0_T_1(M0,E)
+                Omega,e = Omega_e_at_M0_T_1(M0,T)
             else:
                 try:
-                    Omega,e = Omega_e_at_M0_T_0(M0,E)
+                    Omega,e = Omega_e_at_M0_T_0(M0,T)
                 except InterpError:
                     quark = True
-                    E += dt*dm
-                    J,V0 = J_V_at_M0_E_0(M0,E)
-                    E,V1 = E_V_at_M0_J_1(M0,J)
+                    T += dt*dm
+                    J,V0 = J_V_at_M0_T_0(M0,T)
+                    T,V1 = T_V_at_M0_J_1(M0,J)
                     print(f"phase transition: delta V={V1-V0}")
-                    Omega,e = Omega_e_at_M0_T_1(M0,E)
+                    Omega,e = Omega_e_at_M0_T_1(M0,T)
             dm = m2 * Omega**4 / (6*np.pi)
-            E -= dt * dm
+            T -= dt * dm
             yield t,dm,e
             t += dt
     except InterpError:
@@ -116,11 +116,9 @@ fig, (ax1,ax2) = plt.subplots(2,1,sharex=True)
 def plot_res(M0,res):
     res = np.array(res)
     res[:,0] /= s
-    res[:,1:3] /= g*cm**2*s**-3
-    ax1.loglog(res[:,0],res[:,1],label=f"T,$M_0={M0}$")
-    ax1.loglog(res[:,0],res[:,2],label=f"J,$M_0={M0}$")
-    ax2.semilogx(res[:,0],res[:,3],label=f"T,$M_0={M0}$")
-    ax2.semilogx(res[:,0],res[:,4],label=f"J,$M_0={M0}$")
+    res[:,1] /= g*cm**2*s**-3
+    ax1.loglog(res[:,0],res[:,1],label=f"$M_0={M0}$")
+    ax2.semilogx(res[:,0],res[:,2],label=f"$M_0={M0}$")
 
 def run(M0,E):
     res = list(evolve(M0,E))
@@ -138,7 +136,7 @@ def show():
     fig.savefig("res",dpi=500)
 
 if __name__ == '__main__':
-    run(.206,6.337e-5)
+    run(.203,5e-5)
     show()
 """
     run(1.748)
